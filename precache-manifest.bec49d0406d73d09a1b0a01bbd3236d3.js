eab87d282c30b7c6971355096babd3bf84deba7a
Tung Le <tunglt11@outlook.com>
2019-07-16 14:59:20 +0700

    Updates

---
+++ precache-manifest.bec49d0406d73d09a1b0a01bbd3236d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964460d681225653c602ed97cb84ad78",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "ff8529923e8b301b9275",
+    "url": "/react-training/static/css/main.bf7b5b22.chunk.css"
+  },
+  {
+    "revision": "132bc5cc11868f1328aa",
+    "url": "/react-training/static/js/2.aa8e7148.chunk.js"
+  },
+  {
+    "revision": "ff8529923e8b301b9275",
+    "url": "/react-training/static/js/main.30c67756.chunk.js"
+  },
+  {
+    "revision": "4f30c18725475ba92dbe",
+    "url": "/react-training/static/js/runtime~main.a1ad22db.js"
+  }
+]);
