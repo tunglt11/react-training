130956542ba686ffe215e651e90484ef80f42881
Tung Le <tunglt11@outlook.com>
2019-07-16 14:43:10 +0700

    initail commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import FilterableProductTable from './FilterableProductTable';
+import Main from './Main';
+
+import * as serviceWorker from './serviceWorker';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const PRODUCTS = [
+    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
+
+
+
+//ReactDOM.render(<FilterableProductTable products={PRODUCTS} />, document.getElementById('root'));
+ReactDOM.render(<Main/>, document.getElementById('root'));
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
